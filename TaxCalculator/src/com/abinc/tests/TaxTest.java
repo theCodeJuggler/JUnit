b559238ad5a8d205230232646fd5687616d72ea2
Abhirup Biswas <abhirupbiswas100@gmail.com>
2018-08-04 22:29:12 +0530

    small corrections

--- TaxCalculator/src/com/abinc/tests/TaxTest.java
+++ TaxCalculator/src/com/abinc/tests/TaxTest.java
@@ -9,28 +9,36 @@ import com.abinc.app.TaxCalculator;
 
 public class TaxTest {
 
-	//test case to check for non indian nationality
-		@Test
-		public void testcase1() throws Exception {
-		TaxCalculator tax=new TaxCalculator();
-		double answer = tax.calculateTax("Ron",3500.00,false);
-		Assert.assertEquals(0.0, answer,0.0);
-		}
+	// test case to check for non indian nationality
+	@Test
+	public void testIsIndianFalse() throws Exception {
+		TaxCalculator calculator = new TaxCalculator();
+		double ans = calculator.calculateTax("Ron", 34000, false);
+		Assert.assertEquals(0, ans, 0);
+	}
 
-		//test case to check for valid tax calculation
-		@Test
-		public void testcase2() throws Exception {
-		TaxCalculator tax=new TaxCalculator();
-		double answer = tax.calculateTax("Tim",55000.00,true);
-		Assert.assertEquals(3300.00, answer,0.0);
-		}
-		
-		//test case to check for invalid name
-		@Test
-		public void testcase3() throws Exception {
-		TaxCalculator tax=new TaxCalculator();
-		double answer = tax.calculateTax(null,30000.00,true);
-		Assert.assertEquals(0.0, answer,0.0);
+	// test case to check tax not eligible
+	@Test
+	public void testTaxNotEligible() throws Exception {
+		TaxCalculator calculator = new TaxCalculator();
+		double ans = calculator.calculateTax("Tim", 1000, true);
+		Assert.assertEquals(0, ans, 0);
+	}
+
+	// test case to check valid case
+	@Test
+	public void testValidCase() throws Exception {
+		TaxCalculator calculator = new TaxCalculator();
+		double ans = calculator.calculateTax("Jack", 30000, true);
+		Assert.assertEquals(1500, ans, 0);
+	}
+
+	// test case to check name is null or not valid
+	@Test
+	public void testNullName() throws Exception {
+		TaxCalculator calculator = new TaxCalculator();
+		double ans = calculator.calculateTax("", 30000, true);
+		Assert.assertEquals(0, ans, 0);
 	}
 
 }
